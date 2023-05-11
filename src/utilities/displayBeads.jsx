15d0a35405babe1b5b5c69e707f2b57d65e35af3
Caitlin Lamprecht <caitlin.cmo@gmail.com>
2023-05-11 12:54:51 -0500

    updated formatting on displayBeads

--- src/utilities/displayBeads.jsx
+++ src/utilities/displayBeads.jsx
@@ -1,9 +1,9 @@
-import Bead from '../components/Bead'
+import Bead from "../components/Bead";
 
 export function displayBeads(value) {
-    var beads = [];
-		for (let i = 0; i < value; i++) {
-			beads.push("b");
-		}
-		return beads.map((bead, i) => <Bead key={i}/>);
+	var beads = [];
+	for (let i = 0; i < value; i++) {
+		beads.push("b");
+	}
+	return beads.map((bead, i) => <Bead key={i} />);
 }
