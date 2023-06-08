c12e58cb8ef9fc293349a82ec6738615fd1cf24c
Caitlin Lamprecht <caitlin.cmo@gmail.com>
2023-06-08 13:01:26 -0500

    removed react-router

--- src/index.js
+++ src/index.js
@@ -4,13 +4,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<Router>
-		<App />
-	</Router>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 reportWebVitals();
